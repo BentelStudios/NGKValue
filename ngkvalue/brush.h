2da098ea8c9dc14c68735c9126da4f3e87fd2e90
ValueProgrammer <140750964+BentelStudios@users.noreply.github.com>
2024-10-04 15:34:48 -0700

    Update brush.h

--- ngkvalue/brush.h
+++ ngkvalue/brush.h
@@ -5,6 +5,10 @@
 
 #endif
 
+#define BRUSH_POINTS_NUM 0x8
+
+#include "bsp.h"
+
 typedef bool eboolean;
 
 typedef class EBrush {
@@ -18,5 +22,11 @@ public:
 
 	EBrush* EPlane__Ptr;
 	EBrush* eprev, * eowner, * enext;
+
+	void Brush_Shader(const char * pShaderName, EBrush& brush);
+	virtual void Construct_Brush(EBrush * brush, const char * name, unsigned int BrushSpawnflags);
+	void New_Brush(EBrush * new_brush, bool previous_brush);
+	virtual void Add_BrushToBsp(EBrush * brush, const char * name, int brush_leaf, Bsp * bsp_node);
+	void Brush_Mode(EBrush * brush_mode, enum EMode);
 
 };
