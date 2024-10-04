039ca6b4cb4648e3ce0d9d06b692e67935dc75b3
ValueProgrammer <140750964+BentelStudios@users.noreply.github.com>
2024-10-04 16:17:45 -0700

    Update brush.h

--- ngkvalue/brush.h
+++ ngkvalue/brush.h
@@ -27,6 +27,12 @@ public:
 	virtual void Construct_Brush(EBrush * brush, const char * name, unsigned int BrushSpawnflags);
 	void New_Brush(EBrush * new_brush, bool previous_brush);
 	virtual void Add_BrushToBsp(EBrush * brush, const char * name, int brush_leaf, Bsp * bsp_node);
-	void Brush_Mode(EBrush * brush_mode, enum EMode);
-
+	void Brush_Mode(EBrush * brush_mode, enum EMode);	
+	void Split_Brush(EBrush * current, int split_points);
+	void Drag_Brush(EBrush * brush, eboolean brush_selected);
+	void Brush_EPair(EBrush * brush, const char * eKey, const char * eValue);
+	void Brush_Free(EBrush * brush);
+	void Brush_Lock(EBrush * brush);
+	void Brush_Alloc(EBrush * brush);
+	voif Brush_Null(EBrush * NullBrush);
 };
