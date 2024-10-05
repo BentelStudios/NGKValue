bb8af6ad57f6b343f8b42a0c14487a479a53d39f
ValueProgrammer <140750964+BentelStudios@users.noreply.github.com>
2024-10-04 19:22:28 -0700

    Create matrix.h

---
+++ libs/matrixlib/matrix.h
@@ -0,0 +1,21 @@
+/*
+=======================================================
+            MATRIX.H HAS MATRIX FUNCTION
+=======================================================
+*/
+#pragma once
+
+#ifndef MATRIX_H
+#define MATRIX_H
+
+#endif
+
+#include "matrix_identity.h"
+
+class Matrix{
+public:
+
+  int MatrixId = MatrixNumber
+
+
+};
