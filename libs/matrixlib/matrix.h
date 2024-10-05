d94e6f958822df2fed2b3799c16f12af8b2a711d
ValueProgrammer <140750964+BentelStudios@users.noreply.github.com>
2024-10-04 19:47:53 -0700

    Update matrix.h

--- libs/matrixlib/matrix.h
+++ libs/matrixlib/matrix.h
@@ -12,10 +12,39 @@
 
 #include "matrix_identity.h"
 
-class Matrix{
+class Matrix {
 public:
 
-  int MatrixId = MatrixNumber
+    int MatrixId = 1024;
+    int m4x4Point[16];
+    const int MatrixXAxis; const int MatrixYAxis; const int MatrixZAxis;
+    Matrix& mMatCallback_signal;
 
+    void mMatrix_CallbackSignal(Matrix* mMatrixCallbackSignal)
+    {
+        mMatrixCallbackSignal->MatrixId = 1024;
+    };
+
+    void Matrix4x4(Matrix* matrix, Matrix& matcall)
+    {
+        int x[3];
+        int y[3];
+        int z[3];
+
+            matrix->m4x4Point[0], matrix->m4x4Point[1], matrix->m4x4Point[2],
+            matrix->m4x4Point[3], matrix->m4x4Point[4], matrix->m4x4Point[5],
+            matrix->m4x4Point[6], matrix->m4x4Point[7], matrix->m4x4Point[8],
+            matrix->m4x4Point[9], matrix->m4x4Point[10], matrix->m4x4Point[11],
+            matrix->m4x4Point[12], matrix->m4x4Point[13], matrix->m4x4Point[14],
+            matrix->m4x4Point[15];
+            
+            matrix->m4x4Point[0] = x[0]; matrix->m4x4Point[1] = y[0]; matrix->m4x4Point[2] = z[0];
+            matrix->m4x4Point[3] = x[1]; matrix->m4x4Point[4] = y[1]; matrix->m4x4Point[5] = z[1];
+            matrix->m4x4Point[6] = x[2]; matrix->m4x4Point[7] = y[2]; matrix->m4x4Point[8] = z[2];
+    };
+
+    void Matrix3x3(Matrix);
+    void Matrix3x4(Matrix);
+    virtual void MatrixType(Matrix* mMatrixType);
 
 };
