94c6667f6c3812360185a3c2b85e94804f43b36d
ValueProgrammer <140750964+BentelStudios@users.noreply.github.com>
2024-10-04 18:36:36 -0700

    Update bsp.h

--- ngkvalue/bsp.h
+++ ngkvalue/bsp.h
@@ -1,1 +1,24 @@
+/*
+==============================================================
+          BSP.H DEFINE BINARY SPACE PARTITIONING
+==============================================================
+*/
 #pragma once
+
+#ifndef BSP_H
+#define BSP_H
+
+#endif
+
+#define MAX_PLANE_NUMS 35000
+#define MAX_BRUSH_NUMS 30000
+#define MAX_LEAF_BSP_NODES 80000
+#define MAX_BSP_TREE 100000
+
+typedef class Bsp {
+public:
+
+
+
+
+};
