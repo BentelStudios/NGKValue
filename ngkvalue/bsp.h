f422c9b58085353a9e491aeec998aabc935c44e1
ValueProgrammer <140750964+BentelStudios@users.noreply.github.com>
2024-10-04 18:49:19 -0700

    Update bsp.h

--- ngkvalue/bsp.h
+++ ngkvalue/bsp.h
@@ -15,10 +15,32 @@
 #define MAX_LEAF_BSP_NODES 80000
 #define MAX_BSP_TREE 100000
 
+#include "node.h"
+#include "entity.h"
+#include "brush.h"
+
 typedef class Bsp {
 public:
 
+   int          bsp_plane_nums[35000];
+   int          bsp_brush_nums[30000];
+   int          bsp_leaf_nodes[80000];
 
+const static int bsp_tree_num[100000];
 
+   Bsp * bsp;
+
+   bool BSP_ACTIVE;
+
+   inline void Split_BrushNode(EBrush * brush);
+   inline void Add_BrushNode(EBrush * brush, const int brush_num);
+   inline void Add_Node();
+   inline void Add_Entity();
+
+   enum BspType{
+     BSP_CUTTER = 0x0,
+     BSP_DEFINE = 1x1,
+     BSP_SWAP = 2x2
+   };
 
 };
