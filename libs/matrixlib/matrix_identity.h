3b7e9dfa095c4b4298b0dbb5178c5a9e3ac5bef2
ValueProgrammer <140750964+BentelStudios@users.noreply.github.com>
2024-10-04 19:16:49 -0700

    Update matrix_identity.h

--- libs/matrixlib/matrix_identity.h
+++ libs/matrixlib/matrix_identity.h
@@ -1,1 +1,33 @@
+/*
+=================================================================
+        MATRIX_IDENTITY.H CONTAINS MATRIX INFORMATION
+=================================================================
+*/
+#pragma once
+
+#ifndef MATRIX_IDENTITY_H
+#define MATRIX_IDENTITY_H
+
+#endif
+
+#include <math.h>
+
+class MatrixIdentity{
+public:
+
+  MatrixIdentity();
+   virtual ~MatrixIdentity();
+
+  const int MatrixNumber[1024];
+  static int MatrixBuffer[2048];
+
+  bool RefreshMatrix(MatrixIdentity, float mTick);
+
+  enum MatrixIdentity{
+   M4X4 = 0, M3X4 = 1, M3X3 = 2 
+  };
+
+  void LoadIdentity(MatrixIdentity * matrix_identity, int mNumber, static int mBuffer);
+
+};
 
