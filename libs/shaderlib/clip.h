9a49ee1a61e2e0fd2e30b2a3e59ba68383738c1d
ValueProgrammer <140750964+BentelStudios@users.noreply.github.com>
2024-10-05 12:03:44 -0700

    Create clip.h

---
+++ libs/shaderlib/clip.h
@@ -0,0 +1,26 @@
+/*
+============================================
+        CLIP.H BLOCK AI AND PLAYER 
+============================================
+*/
+
+#ifndef CLIP_H
+#define CLIP_H
+
+#endif
+
+#define CLIP_PLAYER 0x0
+#define CLIP_AI 1x0
+
+typedef struct Clip{
+
+  Clip();
+  virtual ~Clip();
+
+bool cDisableCollision = true;
+
+int cClipSpawnflags = 1;
+
+
+
+};
