a50057fc793701395ba36c0fe69e48621666f78a
ValueProgrammer <140750964+BentelStudios@users.noreply.github.com>
2024-10-05 21:13:45 -0500

    Create caulk.h

---
+++ libs/shaderlib/caulk.h
@@ -0,0 +1,31 @@
+/*
+====================================
+       CAULK.H BRUSH TEXTURE
+====================================
+*/
+#pragma once 
+
+#ifndef CAULK_H
+#define CAULK_H
+
+#endif
+
+#include "shader.h"
+
+#define CAULK_SHADER_PIXEL 000x000
+
+typedef struct caulk{
+
+ bool cCaulkBrushShader = true;
+
+ static int cCaulkSpawnflags = 2;
+
+ enum cCaulkColor{
+  CAULK_WHITE = 0,
+  CAULK_PINK = 1,
+  CAULK_YELLOW = 2
+ };
+
+ void cCaulkShader();
+
+};
