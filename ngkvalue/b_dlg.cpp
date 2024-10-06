68a0b23ed99b54d7968db16e7d7fb0239035fe97
ValueProgrammer <140750964+BentelStudios@users.noreply.github.com>
2024-10-06 00:10:09 -0500

    Update b_dlg.cpp

--- ngkvalue/b_dlg.cpp
+++ ngkvalue/b_dlg.cpp
@@ -1,5 +1,7 @@
 #include "b_dlg.h"
 #include <QDialog>
+#include <QString>
+#include <QPushButton>
 
 QWidget qBrushDialog;
 
@@ -11,12 +13,23 @@ static float _ysize;
 int q_bdCode = 6373;
 int q_bdRFlags = 2123;
 
+QString q_bdTitle = "Brush Inspector";
+
 BrushDialog * Create_Dialog(BrushDialog * dlg, QWidget& qBrushDialog){
-  QWidget * qBrushDialogPtr;
+  QWidget * qBrushDialogGlobal;
   qBrushDialog.QDialog(qBrushDialog);
-    qBrushDialogPtr->activateWindow(qBrushDialog.isWindow(qBrushDialog) == true);
-    qBrushDialogPtr->DialogCode{ q_bdCode };
-    qBrushDialogPtr->RenderFlags{ q_bdRFlags };
+    qBrushDialogGlobal->activateWindow(qBrushDialog.isWindow(qBrushDialog) == true);
+    qBrushDialogGlobal->DialogCode{ q_bdCode };
+    qBrushDialogGlobal->RenderFlags{ q_bdRFlags };
 
   return qBrushDialogPtr;
+  
+};
+
+BrushDialog * Dialog_Items(BrushDialog * dlg, QWidget& qBrushDialog, QPushButton& qGlobalButton){
+  QWidget * qBrushDialogGlobal;
+  qBrushDialogGlobal->windowTitle(q_bdTitle);
+    qGlobalButton.QPushButton(text = "Fit", parent = qBrushDialog(qGlobalButton));
+      qGlobalButton.setFlat(qGlobalButton) == true;
+      qGlobalButton.hitButton(false);
 };
