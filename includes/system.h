31f5e97ff779ab04fbba7b0cd6ec0d674ffdf446
ValueProgrammer <140750964+BentelStudios@users.noreply.github.com>
2024-10-06 13:33:22 -0700

    Update system.h

--- includes/system.h
+++ includes/system.h
@@ -1,1 +1,15 @@
+/*
+  SYSTEM.H SYSTEM FUNCTION GROUPS
+*/
+#pragma once
 
+#ifndef SYSTEM_H
+#define SYSTEM_H
+
+#endif
+
+#include <string>
+
+typedef std::string Sys_printf;
+typedef std::string Sys_printi;
+typedef std::string Sys_printc;
