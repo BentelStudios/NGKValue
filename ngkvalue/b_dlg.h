f65132aece17edc1adce94effff7b3e1b3e58688
ValueProgrammer <140750964+BentelStudios@users.noreply.github.com>
2024-10-05 23:39:24 -0500

    Update b_dlg.h

--- ngkvalue/b_dlg.h
+++ ngkvalue/b_dlg.h
@@ -1,1 +1,32 @@
 #pragma once
+
+#ifndef B_DLG_H
+#define B_DLG_H
+
+#endif 
+
+#include "QWidget.h"
+#include "brush.h"
+#include "texture.h"
+#include "patch.h"
+#include "csg.h"
+
+typedef class BrushDialog{
+public:
+
+const int Init_BDlg = NULL;
+
+/*
+DIALOG BUTTON OPTIONS
+*/
+static int FitTexture;
+static int LMapTexture;
+static int SmoothTexture;
+
+double VTextureScale;
+double HTextureScale;
+
+bool IncreaseStretch(Texture);
+bool DecreaseStretch(Texture);
+
+};
