98d24729c2b6712ef083adcc55f54dcab36ca16b
ValueProgrammer <140750964+BentelStudios@users.noreply.github.com>
2024-10-05 23:55:04 -0500

    Update b_dlg.cpp

---
+++ ngkvalue/b_dlg.cpp
@@ -0,0 +1,20 @@
+#include "b_dlg.h"
+#include <QDialog>
+
+QWidget qBrushDialog;
+
+static float _x;
+static float _y;
+static float _xsize;
+static float _ysize;
+
+int q_bdCode = 6373;
+int q_bdRFlags = 2123;
+
+BrushDialog * Create_Dialog(BrushDialog * dlg, QWidget& qBrushDialog){
+  QWidget * qBrushDialogPtr;
+  qBrushDialog.QDialog(qBrushDialog);
+    qBrushDialogPtr->activateWindow(qBrushDialog.isWindow(qBrushDialog) == true);
+    qBrushDialogPtr->DialogCode{ q_bdCode };
+    qBrushDialogPtr->RenderFlags{ q_bdRFlags };
+};
